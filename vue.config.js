e06f86f4465d52d5d4d9f18ffa3277d869c19dfa
Aiden Vigue <contact@vigue.me>
2020-04-24 17:11:30 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,6 @@ module.exports = {
         "appId": "octotouch",
         "name": "OctoTouch",
         "description": "OctoPrint touch panel with material design influences",
-        "homepage": "https://github.com/acvigue/OctoTouch",
-        "repository": "https://github.com/acvigue/OctoTouch",
         "linux": {
           "target": [
             "AppImage",
