e8726525dfd9ae3f04252f4cd89bb90b06f4bcd0
Aiden Vigue <9821808+acvigue@users.noreply.github.com>
2020-04-25 19:20:54 -0400

    Control screen is fully responsive

--- src/Views/Control/Control.vue
+++ src/Views/Control/Control.vue
@@ -6,64 +6,64 @@
     <div fill-height fluid class="container">
         <v-row class="header">
             <v-col @click="goto('/')" class="header-item" align="start">
-                <span style="float: left; margin-left: 2vw;"><v-icon style="margin-top: -3px;" color="#fff" size="40px">mdi-arrow-left-circle-outline</v-icon></span>
+                <span style="float: left; margin-left: 2vw;"><v-icon style="margin-top: -3px;" color="#fff" size="8.33vh">mdi-arrow-left-circle-outline</v-icon></span>
             </v-col>
             <v-col class="header-item" align="center">
-                <v-icon style="margin-top: -3px;" size="40px" color="#fff">mdi-google-controller</v-icon>
+                <v-icon style="margin-top: -0.625vh;" size="8.33vh" color="#fff">mdi-google-controller</v-icon>
             </v-col>
             <v-col class="header-item" align="end">
                 <span style="float: right; margin-right: 5vw;">{{ printer.name }}</span>
             </v-col>
         </v-row>
         <v-row class="control__content">
-            <v-col :sm="5" class="control__directional-pad">
-                <v-row style="height: 107px;">
+            <v-col cols="5" class="control__directional-pad">
+                <v-row style="height: 22.2vh;">
                     <v-col></v-col>
-                    <v-col v-ripple @click="jog('y', 10)"><v-icon style="margin-top: 5px;" size="20vh" color="#fff">mdi-arrow-up-thick</v-icon></v-col>
-                    <v-col></v-col>
-                </v-row>
-                <v-row style="height: 107px;">
-                    <v-col @click="jog('x', -10)" v-ripple><v-icon size="20vh" color="#fff">mdi-arrow-left-thick</v-icon></v-col>
-                    <v-col @click="action('home')" v-ripple><v-icon style="margin: auto; top: 15%; width: 7vw;" size="14vh" color="#fff">mdi-home</v-icon></v-col>
-                    <v-col @click="jog('x', 10)" v-ripple><v-icon size="20vh" color="#fff">mdi-arrow-right-thick</v-icon></v-col>
-                </v-row>
-                <v-row style="height: 107px;">
-                    <v-col></v-col>
-                    <v-col @click="jog('y', -10)" v-ripple><v-icon style="margin-bottom: 5px;" size="20vh" color="#fff">mdi-arrow-down-thick</v-icon></v-col>
+                    <v-col v-ripple @click="jog('y', 10)"><v-icon style="margin-top: 1.04vh;" size="20vh" color="#fff">mdi-arrow-up-thick</v-icon></v-col>
                     <v-col></v-col>
                 </v-row>
-                <v-row style="height: 107px;">
+                <v-row style="height: 22.2vh;">
+                    <v-col cols="5" @click="jog('x', -10)" v-ripple><v-icon size="20vh" color="#fff">mdi-arrow-left-thick</v-icon></v-col>
+                    <v-col cols="2" @click="action('home')" v-ripple><v-icon style="margin: auto; top: 20%; margin-left: -1.9vw;" size="7vw" color="#fff">mdi-home</v-icon></v-col>
+                    <v-col cols="5" @click="jog('x', 10)" v-ripple><v-icon size="20vh" color="#fff">mdi-arrow-right-thick</v-icon></v-col>
+                </v-row>
+                <v-row style="height: 22.2vh;">
                     <v-col></v-col>
-                    <v-col style="text-align: center; color: #636e72; font-size: 30px; font-weight: 300;">X / Y</v-col>
+                    <v-col @click="jog('y', -10)" v-ripple><v-icon style="margin-bottom: 1.04vh;" size="20vh" color="#fff">mdi-arrow-down-thick</v-icon></v-col>
                     <v-col></v-col>
                 </v-row>
-            </v-col>
-            <v-col :sm="3">
-                <v-row style="height: 107px;">
+                <v-row style="height: 22.2vh;">
                     <v-col></v-col>
-                    <v-col @click="jog('z', 10)" v-ripple><v-icon style="margin-top: 5px;" size="20vh" color="#fff">mdi-arrow-up-thick</v-icon></v-col>
-                    <v-col></v-col>
-                </v-row>
-                <v-row style="height: 107px;">
-                    <v-col v-ripple></v-col>
-                    <v-col v-ripple></v-col>
-                    <v-col v-ripple></v-col>
-                </v-row>
-                <v-row style="height: 107px;">
-                    <v-col></v-col>
-                    <v-col @click="jog('z', -10)" v-ripple><v-icon style="margin-bottom: 5px;" size="20vh" color="#fff">mdi-arrow-down-thick</v-icon></v-col>
-                    <v-col></v-col>
-                </v-row>
-                <v-row style="height: 107px;">
-                    <v-col></v-col>
-                    <v-col style="text-align: center; color: #636e72; font-size: 30px; font-weight: 300;">Z</v-col>
+                    <v-col style="text-align: center; color: #636e72; font-size: 6.25vh; font-weight: 300;">X / Y</v-col>
                     <v-col></v-col>
                 </v-row>
             </v-col>
-            <v-col :sm="4" style="margin-top: 3vh; padding-right: 5vw;">
+            <v-col cols="3">
+                <v-row style="height: 22.2vh;">
+                    <v-col></v-col>
+                    <v-col @click="jog('z', 10)" v-ripple><v-icon style="margin-top: 1.04vh;" size="20vh" color="#fff">mdi-arrow-up-thick</v-icon></v-col>
+                    <v-col></v-col>
+                </v-row>
+                <v-row style="height: 22.2vh;">
+                    <v-col v-ripple></v-col>
+                    <v-col v-ripple></v-col>
+                    <v-col v-ripple></v-col>
+                </v-row>
+                <v-row style="height: 22.2vh;">
+                    <v-col></v-col>
+                    <v-col @click="jog('z', -10)" v-ripple><v-icon style="margin-bottom: 1.04vh;" size="20vh" color="#fff">mdi-arrow-down-thick</v-icon></v-col>
+                    <v-col></v-col>
+                </v-row>
+                <v-row style="height: 22.2vh;">
+                    <v-col></v-col>
+                    <v-col style="text-align: center; color: #636e72; font-size: 6.25vh; font-weight: 300;">Z</v-col>
+                    <v-col></v-col>
+                </v-row>
+            </v-col>
+            <v-col cols="4" style="margin-top: 3vh; padding-right: 0vw;">
                 <div class="control__detail-item">
                     <v-row v-ripple @click="overlay = 'tempAdjust-hotend'">
-                        <v-col :sm="4">
+                        <v-col cols="4">
                             <v-icon size="15vh" color="#ff7675">mdi-printer-3d-nozzle</v-icon>
                         </v-col>
                         <v-col style="margin-top: 2vh;">
@@ -72,10 +72,10 @@
                         </v-col>
                     </v-row>
                 </div>
-                <br />
+                <br>
                 <div class="control__detail-item">
                     <v-row v-ripple @click="overlay = 'tempAdjust-heatedbed'">
-                        <v-col :sm="4">
+                        <v-col cols="4">
                             <v-icon size="15vh" color="#00b894">mdi-radiator</v-icon>
                         </v-col>
                         <v-col style="margin-top: 2vh;">
@@ -84,14 +84,14 @@
                         </v-col>
                     </v-row>
                 </div>
-                <br />
+                <br>
                 <div class="control__detail-item">
                     <v-row>
-                        <v-col :sm="4">
+                        <v-col cols="4">
                             <v-icon size="15vh" :class="spinning" color="#0984e3">mdi-fan</v-icon>
                         </v-col>
                         <v-col style="margin-top: 3vh;">
-                            <span class="control__detail-text" style="margin-top: 7px;">{{ Math.round(printer.fan.speed) }}%</span>
+                            <span class="control__detail-text" style="margin-top: 1.45vh;">{{ Math.round(printer.fan.speed) }}%</span>
                         </v-col>
                     </v-row>
                 </div>
@@ -129,7 +129,7 @@ export default Vue.extend({
             this.getJobStatus().then((data) => {
                 this.printer.state = data.state
                 if (data.state == "Printing" || data.state == "Starting") {
-                    this.$router.push("/now-printing")
+                    //this.$router.push("/now-printing")
                 }
             })
             this.getLayerStatus().then((data) => {
